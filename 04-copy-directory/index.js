fc774c979afe1a94e7e79e8fb23611c8ccf13219
Oleggdl <oblotskiyj@gmail.com>
2021-11-04 18:22:41 +0300

    completed 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require('fs')
+const path = require('path')
+const fsPromises = fs.promises
 
+const srcDir = path.join(__dirname, 'files')
+const destDir = path.join(__dirname, 'files-copy')
+
+
+const copyDirectory = () => {
+
+    fs.access(destDir, (error) => {
+        if (error) {
+            fsPromises.mkdir(destDir, { recursive: true })
+                .catch(error => console.log(error));
+        }
+
+        fsPromises.readdir(srcDir, { withFileTypes: true })
+            .then(res => {
+                res.forEach(e => {
+                    fsPromises.copyFile(path.join(srcDir, e.name), path.join(destDir, e.name))
+                })
+            })
+            .catch(error => console.log(error))
+    })
+}
+
+
+copyDirectory()
